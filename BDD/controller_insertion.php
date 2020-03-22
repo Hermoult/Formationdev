60f5a48e664f104b7cfedcd5b7e488fbcbbc1537
Hermoult <62248355+Hermoult@users.noreply.github.com>
2020-03-22 18:39:36 +0100

    Create controller_insertion.php

---
+++ BDD/controller_insertion.php
@@ -0,0 +1,23 @@
+<?php
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=insertion_sql', 'phpmyadmin', 'Adher57070');
+} catch (\Throwable $th) {
+    die('error sql connection');
+}
+
+
+$firstname = htmlspecialchars($_POST['firstname']);
+$name = htmlspecialchars($_POST['name']);
+$password = htmlspecialchars($_POST['password']);
+
+echo '<br/>'.'Bonjour '. $firstname.' '.$name.' '.' votre mot de pass est'.' '.$password;
+
+$sql = "
+INSERT INTO user(name, firstname, password) 
+VALUES (?,?,?)
+";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$firstname, $name, $password]);
+
+?>
