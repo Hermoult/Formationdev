9086b7887327092e2808c743c7eddb8ce41b456a
Hermoult <62248355+Hermoult@users.noreply.github.com>
2020-03-24 16:47:30 +0100

    Update create_ctrl.php

--- CRUD/controllers/create_ctrl.php
+++ CRUD/controllers/create_ctrl.php
@@ -5,22 +5,17 @@ $password = htmlspecialchars($_POST['password']);
 $description = htmlspecialchars($_POST['description']);
 // Connexion a la base via PDO
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=crud', 'phpmyadmin', 'Adher57070');
+    $pdo = new PDO('mysql:host=localhost;dbname=crud', 'phpmyadmin', 'Adher57070', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     echo 'connexion réussie' . '<br/>';
 } catch (\Throwable $th) {
     die('error sql connection');
     echo 'connexion échouée' . '<br/>';
 }
 echo '<br/>'.'Bonjour '. $pseudo.' votre mot de pass est '.$password.'<br/>';
-// Création de la commande sql
-$sql = '
-INSERT INTO user(pseudo, password, description) 
-VALUES (?,?,?)
-';
-// Preparation et execution de la requete de la commande sql via PDO
+// Preparation et execution de la commande sql
+$sql = 'INSERT INTO user(pseudo, password, description) VALUES (?,?,?)';
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$pseudo,$password,$description]);
-
 // Retour client sur navigateur
 echo 'Votre profil a été créé avec Succès !';
 ?>
