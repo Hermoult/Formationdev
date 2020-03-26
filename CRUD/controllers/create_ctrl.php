9ef0ea62a09ad36d437c2c954f51fe89f605a047
Hermoult <62248355+Hermoult@users.noreply.github.com>
2020-03-26 10:59:43 +0100

    Update create_ctrl.php

--- CRUD/controllers/create_ctrl.php
+++ CRUD/controllers/create_ctrl.php
@@ -9,7 +9,7 @@ $description = htmlspecialchars($_POST['description']);
 // Connexion a la base via PDO
 
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=crud', 'phpmyadmin', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $pdo = new PDO('mysql:host=localhost;dbname=crud', 'adrien', 'adrien', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (\Throwable $th) {
     die('error sql connection');
 }
