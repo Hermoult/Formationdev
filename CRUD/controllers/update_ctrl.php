a83459283d0604249484e2ecd439b27bd6cf5201
Hermoult <adrienhergat@gmail.com>
2020-03-26 15:10:10 +0100

    File update finish

--- CRUD/controllers/update_ctrl.php
+++ CRUD/controllers/update_ctrl.php
@@ -6,7 +6,6 @@ if (isset($_POST['pseudo']))
     $password = htmlspecialchars($_POST['password']);
     $description = htmlspecialchars($_POST['description']);
     
-    
     // Variables Database
     $DB_NAME = "crud";
     $DB_DSN = "mysql:host=localhost;dbname=".$DB_NAME;
@@ -21,24 +20,22 @@ if (isset($_POST['pseudo']))
         die('error sql connection');
     } 
 
-    // Preparation, envoie de la commande sql et lecture du profil corespondant
+    // Lecture du profil
     
     $sqls = 'SELECT * FROM user WHERE pseudo = ?';
     $sqlu ='UPDATE user SET password = ?, description = ? WHERE pseudo = ?';
-    try {
-        $stmt = $pdo->prepare($sqls);
-        $stmt-> execute([$pseudo]);
-        $donnees = $stmt->fetch();
-        var_dump($donnees['pseudo']);
-        } catch (\Throwable $th) {
-            die('error reading');
-            }
-    try {
+    
+    $stmt = $pdo->prepare($sqls);
+    $stmt-> execute([$pseudo]);
+    $profil = $stmt ->fetch();
+        
+    if ($profil == null){
+        echo "Pas d'utilisateurs correspondant <br>";
+    } else {
+        $stmt ->closeCursor();
         $stmt = $pdo->prepare($sqlu);
         $stmt -> execute([$password,$description,$pseudo]);
-        $donnees = $stmt->fetch();
-        var_dump($donnees);
-        } catch (\Throwable $th) {
-            die('error updating');
-            }
+        echo "Le profil $pseudo a été modifié avec succès, nouvau mot de pass : \"$password\", nouvelle description : \"$description\".";
+    }     
 }
+?>
