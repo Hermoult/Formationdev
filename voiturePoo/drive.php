b10f86c18b9544299913bc2cf0a4bdbe3033d7ac
Hermoult <adrienhergat@gmail.com>
2020-04-09 09:18:59 +0200

    ajout

---
+++ voiturePoo/drive.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>  
+    <title>Démarage du véhicule</title>
+</head>
+<body>
+    <div class="jumbotron">
+        <h1 class="display-3">Tesla</h1>
+        <p class="lead"> Electric vehicle</p>
+        <hr class="my-2">
+        <p><?php
+            require 'vehicle.php';
+            $vehicle1 = new vehicle(TRUE,80,vehicle::LOW_ENGINE_POWER);
+            $vehicle1 -> engine_start();
+            $vehicle1 -> affichage();
+            ?>
+        </p>
+        <p class="lead">
+            <a class="btn btn-primary btn-lg" href="drive.php" role="button">Start</a>
+        </p>
+</div>
+</body>
+</html>
